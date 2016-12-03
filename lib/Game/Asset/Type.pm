9bad18c5afb643580469aa6ef2a03138b187fe0c
tmurray <tmurray@wumpus-cave.net>
2016-12-03 08:30:53 -0600

    Add blank line for automatic VERSION

--- lib/Game/Asset/Type.pm
+++ lib/Game/Asset/Type.pm
@@ -22,6 +22,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
 # POSSIBILITY OF SUCH DAMAGE.
 package Game::Asset::Type;
+
 use strict;
 use warnings;
 use Moose::Role;
